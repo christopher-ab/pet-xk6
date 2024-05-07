f7da82d194be3c822b5fa55d58a24ad815d644fe
Christopher Tok <christopher.tok@accelbyte.net>
2024-05-07 13:38:13 +0700

    download user file s3

--- main.go
+++ main.go
@@ -26,10 +26,13 @@ func (pet *PETUtil) Test() (res string, err error) {
 		}
 		return
 	}
+	fmt.Println("masuk 1")
 	file, err := os.Create(fmt.Sprintf("/root/%s", constant.EnvKeyS3UserFileName))
 	if err != nil {
 		return
 	}
+	fmt.Println("masuk 2")
+
 	defer file.Close()
 	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(os.Getenv(constant.EnvKeyAwsRegion)),
@@ -37,6 +40,8 @@ func (pet *PETUtil) Test() (res string, err error) {
 	if err != nil {
 		return
 	}
+	fmt.Println("masuk 3")
+
 	downloader := s3manager.NewDownloader(awsSession)
 	_, err = downloader.Download(file,
 		&s3.GetObjectInput{
@@ -46,6 +51,7 @@ func (pet *PETUtil) Test() (res string, err error) {
 	if err != nil {
 		return
 	}
+	fmt.Println("masuk 4")
 
 	res = "OK"
 	return
